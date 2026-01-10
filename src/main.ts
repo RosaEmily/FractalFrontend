b79e48c6e0694ee3cdb2d2f8920997a72291f897
Sandro <sadaquisa2000@hotmail.com>
2026-01-10 10:14:25 -0500

    feat: install primevue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
 
-import './style.css'
+import "./style.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+app.mount("#app");
