c75d847ff4b2442077c2b7df9a683b043ed84e81
RosaEmily <erodriguez@ide-solution.com>
2026-02-18 21:58:42 -0500

    Add multipages

--- src/services/landing/general.service.ts
+++ src/services/landing/general.service.ts
@@ -2,15 +2,15 @@ import { BaseService } from '../base.service'
 import { api } from '../api'
 import { LANDING_ENDPOINTS } from '../api.routes'
 import type { LandingGeneralData, LandingGeneralResponse } from '@/types/response/general'
-import type { ILandingRepository } from '@/interfaces/landing.repository'
+import type { ILandingGeneralRepository } from '@/interfaces/general.repository'
 import type { ApiError } from '@/types/response/api'
 
-export class LandingGeneralService extends BaseService implements ILandingRepository {
+export class LandingGeneralService extends BaseService implements ILandingGeneralRepository {
   constructor() {
     super(api)
   }
 
-  async fetchLanding(): Promise<LandingGeneralData> {
+  async fetchLandingGeneralData(): Promise<LandingGeneralData> {
     const res = await this.get<LandingGeneralResponse>(LANDING_ENDPOINTS.general)
     if (!res.success) throw res.error as ApiError
     return res.data
